cc89d5d64a4f51a886165219a12268cd3e1d8c65
6jores <noubissi.jores@gmail.com>
2020-03-09 13:37:02 +0000

    FileManager Test

--- app/src/main/java/com/example/positivethinking/MainActivity.java
+++ app/src/main/java/com/example/positivethinking/MainActivity.java
@@ -3,16 +3,30 @@ package com.example.positivethinking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.positivethinking.model.ModelManager;
+import com.example.positivethinking.model.PositiveApp;
+import com.example.positivethinking.model.Thought;
+import com.example.positivethinking.tool.FileManager;
+
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private File appFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fileInitialise();
 
         Button listButton = findViewById(R.id.list_button);
         Button rollButton = findViewById(R.id.roll_button);
@@ -31,8 +45,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ModelManager.setPositiveApp(appFile);
+    }
 
+    private void fileInitialise(){
+        File root = new File(Environment.getExternalStorageDirectory(),"positiveAppRoot");
+        if (!root.exists()){
+            root.mkdirs();
+        }
+        appFile = new File(root,"data");
+    }
 
+    private void testText(){
 
     }
 }
--- app/src/main/java/com/example/positivethinking/model/ModelManager.java
+++ app/src/main/java/com/example/positivethinking/model/ModelManager.java
@@ -1,10 +1,15 @@
 package com.example.positivethinking.model;
 
-abstract public class ModelManager {
+import com.example.positivethinking.tool.FileManager;
+
+import java.io.File;
+
+public class ModelManager {
+
     private static PositiveApp positiveApp;
 
-    public void setPositiveApp(PositiveApp _positiveApp) {
-        positiveApp = _positiveApp;
+    public static PositiveApp getPositiveApp() {
+        return positiveApp;
     }
 
     public static int getId(){
@@ -12,6 +17,24 @@ abstract public class ModelManager {
             return positiveApp.getLastId();
         else
             return 0;
+    }
+
+    public static void setPositiveApp(File file){
+        positiveApp = (PositiveApp)FileManager.readAnObject(file);
+        if (positiveApp == null){
+            setDataTest(file);
+        }
+    }
+
+    private static void setDataTest(File file){
+        positiveApp = new PositiveApp();
+        positiveApp.getThoughts().add(new Thought("Think Positive","Times new roman"));
+        positiveApp.getThoughts().add(new Thought("One day or Day on","Times new roman"));
+        positiveApp.getThoughts().add(new Thought("Try to know what happens if you don't give up","Times new roman"));
+
+        if (FileManager.writeFile(positiveApp,file))
+            positiveApp = (PositiveApp)FileManager.readAnObject(file);
+
     }
 
 }
